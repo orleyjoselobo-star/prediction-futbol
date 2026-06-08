14a05e70e514f544d79f92cdf48b82a8e40d1e2f
orleyjoselobo-star <orleyjoselobo@gmail.com>
2026-06-08 18:28:52 -0500

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,5 @@
-"""Utility functions"""
+"""Utility functions and helpers for the football prediction application."""
+
+from src.utils.helpers import format_match_info
+
+__all__ = ["format_match_info"]
